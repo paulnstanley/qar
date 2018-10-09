068122e199bcc2a7da84a76274eac41fdc7ae765
shodan993 <paulnstanley@gmail.com>
2018-10-09 09:30:36 -0400

    adding question component, test html

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import logo from './QarLogo.png';
+import Questions from './components/Questions.js';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      answers: [],
+      database: []
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,8 +21,18 @@ class App extends Component {
           <h1 className="App-title">Qar</h1>
         </header>
         <p className="App-intro">
-          Need advice for your next car purchase?  Answer these questions to get a model recommendation.
+          Cars are expensive.  In 2018, the average new car and truck sale price was $36,270.  Loan lengths are on average nearly six years, with a $523 payment.  Yikes!
+          Need a suggestion for a nice used car, instead?  Answer these questions to get a recommendation from a thoughfully curated selection.
         </p>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-2"></div>
+            <div className="col-md-8">
+              <Questions />
+              </div>
+            <div className="col-md-2"></div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ server/client/src/components/Questions.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Questions extends Component {
+  render() {
+    return (
+      <div className="Question-form">
+        <p className="test-question">
+        This is a test question?
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Questions;
