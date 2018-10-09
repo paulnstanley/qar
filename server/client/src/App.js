53d866a410e969ec2eec19df6397ab27c8fd5506
shodan993 <paulnstanley@gmail.com>
2018-10-09 09:40:55 -0400

    tweaking app header

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -17,12 +17,22 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Qar</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3">
+              <img src={logo} className="App-logo" alt="logo" />
+              </div>
+            <div className="col-md-6">
+              <h1 className="App-title">Qar</h1>
+              </div>
+            <div className="col-md-3"></div>
+          </div>
+        </div>
         </header>
         <p className="App-intro">
-          Cars are expensive.  In 2018, the average new car and truck sale price was $36,270.  Loan lengths are on average nearly six years, with a $523 payment.  Yikes!
-          Need a suggestion for a nice used car, instead?  Answer these questions to get a recommendation from a thoughfully curated selection.
+          New cars are expensive.  In 2018, the average new car and truck sale price was $36,270, and loan lengths are on average nearly six years.  Yikes!
+          How about a nice used car, instead?  Answer these questions to get a recommendation from a thoughfully curated selection.
+          I'm a car enthusiast on a budget who has bought and sold 21 cars in 16 years, from beater Mitsubishis to BMWs and Porsches.
         </p>
         <div className="container">
           <div className="row">
