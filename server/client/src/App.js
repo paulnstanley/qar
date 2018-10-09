c1fb5a833162faa12323a396d3f3bfc563271f0b
shodan993 <paulnstanley@gmail.com>
2018-10-09 10:25:37 -0400

    testing react-radio-buttons on Questions.js

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -20,13 +20,13 @@ class App extends Component {
         <header className="App-header">
         <div className="container">
           <div className="row">
-            <div className="col-md-2">
+            <div className="col-md-1">
               <img src={logo} className="App-logo" alt="logo" />
               </div>
-            <div className="col-md-8">
+            <div className="col-md-10">
               <h1 className="App-title">Qar</h1>
               </div>
-            <div className="col-md-2"></div>
+            <div className="col-md-1"></div>
           </div>
         </div>
         </header>
@@ -34,12 +34,14 @@ class App extends Component {
           <div className="row">
             <div className="col-md-2"></div>
             <div className="col-md-8">
+              <p/>
               <p className="App-intro">
                 New cars are expensive.  In 2018, the average new car and truck sale price was $36,270, and loan lengths are on average nearly six years.  Yikes!
 
-                How about a nice used car, instead?  Answer these questions to get a recommendation from a thoughfully curated selection.
-                
-                I'm a car enthusiast on a budget who has bought and sold 21 cars in 16 years.
+                How about a nice used car, instead?  Answer these questions to get a recommendation from a thoughtfully curated selection.
+
+                I'm a car enthusiast on a budget who has bought and sold 21 cars in 16 years.  I've written for a car blog, driven Laguna Seca and the Nordschleife,
+                and crammed my wife and kids into a Porsche 911 for weekend trips to Big Sur. 
                 </p>
               </div>
             <div className="col-md-2"></div>
--- server/client/src/components/Questions.js
+++ server/client/src/components/Questions.js
@@ -1,16 +1,66 @@
 import React, { Component } from 'react';
+import { RadioGroup, RadioButton } from 'react-radio-buttons';
 import '../App.css';
 
-class Questions extends Component {
+// class Questions extends Component {
+//   render() {
+//     return (
+//       <div className="Question-form">
+//         <p className="test-question">
+//         This is a test question?
+//         </p>
+//       </div>
+//     )
+//   }
+// }
+
+let App = React.createClass({
+  onChange(value) {
+    console.log(value);
+  },
   render() {
     return (
-      <div className="Question-form">
-        <p className="test-question">
-        This is a test question?
-        </p>
+      <div style={ { padding: 16 } }>
+        <h4>Vertical Radio Buttons</h4>
+        <RadioGroup onChange={ this.onChange }>
+          <RadioButton value="apple">
+            Apple
+          </RadioButton>
+          <RadioButton value="orange">
+            Orange
+          </RadioButton>
+          <ReversedRadioButton value="melon">
+            Melon
+          </ReversedRadioButton>
+        </RadioGroup>
+        <h4 style={ { marginTop: 32 } }>Horizontal Radio Buttons</h4>
+        <RadioGroup onChange={ this.onChange } horizontal>
+          <RadioButton value="apple">
+            Apple
+          </RadioButton>
+          <RadioButton value="orange">
+            Orange
+          </RadioButton>
+          <ReversedRadioButton value="melon">
+            Melon
+          </ReversedRadioButton>
+        </RadioGroup>
+        <h4 style={ { marginTop: 32 } }>1st option disabled (prevents onChange callback and style overwrite to look disabled)</h4>
+        <RadioGroup onChange={ this.onChange }>
+          <RadioButton value="apple" disabled={true} disabledColor={'green'}>
+            Apple
+          </RadioButton>
+          <RadioButton value="orange">
+            Orange
+          </RadioButton>
+          <RadioButton value="melon">
+            Melon
+          </RadioButton>
+        </RadioGroup>
+        <h4>Check console to see the value of the RadioGroup via the onChange callback</h4>
       </div>
-    )
+    );
   }
-}
+});
 
 export default Questions;
