2e02a5f1dc8d07df42926745ecb804aeaef53ad9
shodan993 <paulnstanley@gmail.com>
2018-10-12 15:05:42 -0400

    results rendering

--- server/client/src/components/Questions.js
+++ server/client/src/components/Questions.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+// import {Route} from 'react-router-dom';
 import Select from 'react-select';
 import '../App.css';
 
--- server/client/src/components/RecPage.js
+++ server/client/src/components/RecPage.js
@@ -1,29 +1,14 @@
 import React from 'react';
 
-const RecPage = (props) => (
-  <div>
-    <h1>RecPage stateless component</h1>
-    <h2>{console.log(props)}</h2>
-    <h2>{console.log(props.results[0].data[0])}</h2>
-  </div>
-)
-
-// class RecPage extends Component {
-//   constructor() {
-//     super()
-//
-//   }
-//
-//
-//
-// render() {
-//     return (
-//       <div>
-//         Results:
-//         <h1>{JSON.stringify(this.props.state)}</h1>
-//       </div>
-//     )
-//   }
-// }
+//create vars and text for each property of the car
+const RecPage = (props) => {
+  const make = props.results[0].data[0].make;
+  return (
+    <div className='results'>
+      <h1>Your results:</h1>
+      <h2>{make}</h2>
+    </div>
+  )
+}
 
 export default RecPage;
