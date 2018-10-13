5643c7ba11f5fb0c27bf70e920013e541cceb723
shodan993 <paulnstanley@gmail.com>
2018-10-13 19:26:04 -0400

    simplified sort logic

--- server/datastore/Car_datastore.js
+++ server/datastore/Car_datastore.js
@@ -37,7 +37,8 @@ const MatchCar = function (query, request, response) {
     where('avgCost').lt(parsedQuery.budget).
     where('avgCost').gt(parsedQuery.budget - 5000).
     where('familySize').equals(parsedQuery.passengers).
-    sort('totalScore' + (factor*2) + ('prius'*10));
+    // sort('totalScore'+ (factor*2) + ('prius'*10));
+    sort('totalScore');
 }
 
 const AddNewCar = function (carModel) {
